3b42b1c3bfbca10735e5702dde22e4b5e5380c03
Md. Ashraful Islam <mdashrafulislam.inbox@gmail.com>
2021-11-28 23:50:50 +0600

    git_init

---
+++ gg2/src/main/java/com/example/gg2/gg2.java
@@ -0,0 +1,20 @@
+package com.example.gg2;
+
+import android.graphics.drawable.GradientDrawable;
+import android.view.View;
+
+public class gg2 {
+
+
+    public int sum(int i,int j){
+
+
+        int res = i+j;
+        return res;
+
+
+
+    }
+
+
+}
